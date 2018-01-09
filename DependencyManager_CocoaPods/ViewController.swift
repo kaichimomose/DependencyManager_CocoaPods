a3ba375fa97b728fe3850358fe78a6f22b7bef6f
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2018-01-08 16:58:38 -0800

    first commit

--- DependencyManager_CocoaPods/ViewController.swift
+++ DependencyManager_CocoaPods/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import UIKit
+import RxSwift
+import RxGesture
 
+class ViewController: UIViewController {
+    @IBOutlet weak var container:UIImageView!
+    let disposeBag = DisposeBag()
+    var startPoint = CGPoint.zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
+        self.initPanObserver()
+        self.initLongPressedObserver()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // to draw
+    func initPanObserver(){
+        let observer = self.container.rx.panGesture().share(replay: 1)
+        observer
+            .when(.began)
+            .asLocation()
+            .subscribe(onNext: { [unowned self] location in
+                self.startPoint = location
+            }).disposed(by: self.disposeBag)
+        observer
+            .when(.changed)
+            .asLocation()
+            .subscribe(onNext: { [unowned self] location in
+                self.drawLine(fromPoint: self.startPoint, toPoint: location)
+                self.startPoint = location
+            }).disposed(by: self.disposeBag)
     }
-
-
+    
+    // to erase all
+    func initLongPressedObserver(){
+        let observer = self.container.rx.longPressGesture().share(replay: 1)
+        observer
+            .when(.began)
+            .subscribe(onNext: {[unowned self] _ in
+                self.container.image = nil
+            }).disposed(by: self.disposeBag)
+    }
+    
+    func drawLine(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        self.container.image?.draw(in: CGRect.init(origin: CGPoint.zero, size: self.view.frame.size))
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        context.setLineCap(.round)
+        context.setLineWidth(5)
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.setBlendMode(.normal)
+        context.strokePath()
+        self.container.image = UIGraphicsGetImageFromCurrentImageContext()
+        self.container.alpha = 1.0
+        UIGraphicsEndImageContext()
+    }
 }
-
---
+++ Pods/Target Support Files/Pods-DependencyManager_CocoaPods/Pods-DependencyManager_CocoaPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DependencyManager_CocoaPods : NSObject
+@end
+@implementation PodsDummy_Pods_DependencyManager_CocoaPods
+@end
